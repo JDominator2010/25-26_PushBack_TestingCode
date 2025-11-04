878237ca58f09d8fbfe916334b5f90b40f71b8f1
Jackson Norber <jacksonnorber10@gmail.com>
2025-11-04 17:46:47 -0600

    Update ports for wire management

--- include/devices.h
+++ include/devices.h
@@ -1,15 +1,27 @@
 #pragma once
 #include "main.h"
 #include "pros/adi.hpp"
+#include "pros/imu.hpp"
 
 using namespace pros;
 extern Controller controller;
+
+// -- SENSORS -- //
+extern Imu inertial;
+
+// -- INTAKE MOTORS -- //
 extern Motor Low;
 extern Motor Mid;
 extern Motor High;
+
+// -- DRIVE MOTORS -- //
 extern Motor FL;
 extern Motor FR;
 extern Motor RR;
 extern Motor RL;
 
+
+// -- ADI PORTS -- //
 extern adi::Pneumatics matchLoad;
+
+
--- src/devices.cpp
+++ src/devices.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "pros/adi.hpp"
+#include "pros/imu.hpp"
 #include "pros/misc.h"
 #include "pros/misc.hpp"
 #include "devices.h"
@@ -7,15 +8,21 @@
 using namespace pros;
 
 Controller controller(E_CONTROLLER_MASTER);
+
+// -- SENSORS -- //
+Imu inertial(0);
+
 // -- INTAKE MOTORS -- //
-Motor Low(-13, v5::MotorGears::blue, pros::v5::MotorEncoderUnits::deg);
-Motor Mid(12, v5::MotorGears::blue, pros::v5::MotorEncoderUnits::deg);
-Motor High(-11, v5::MotorGears::blue, pros::v5::MotorEncoderUnits::deg);
+Motor Low(-4, v5::MotorGears::blue, pros::v5::MotorEncoderUnits::deg);
+Motor Mid(5, v5::MotorGears::blue, pros::v5::MotorEncoderUnits::deg);
+Motor High(-6, v5::MotorGears::blue, pros::v5::MotorEncoderUnits::deg);
 
 // -- DRIVE MOTORS -- //
-Motor FL(-16, v5::MotorGears::green, pros::v5::MotorEncoderUnits::deg);
-Motor FR(17, v5::MotorGears::green, pros::v5::MotorEncoderUnits::deg);
-Motor RR(-20, v5::MotorGears::green, pros::v5::MotorEncoderUnits::deg);
-Motor RL(-19, v5::MotorGears::green, pros::v5::MotorEncoderUnits::deg);
+Motor FL(-7, v5::MotorGears::green, pros::v5::MotorEncoderUnits::deg);
+Motor FR(8, v5::MotorGears::green, pros::v5::MotorEncoderUnits::deg);
+Motor RR(-9, v5::MotorGears::green, pros::v5::MotorEncoderUnits::deg);
+Motor RL(-10, v5::MotorGears::green, pros::v5::MotorEncoderUnits::deg);
 
+
+// -- ADI PORTS -- //
 adi::Pneumatics matchLoad('A', false);
